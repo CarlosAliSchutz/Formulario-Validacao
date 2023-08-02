70c888b03a27c530f2cc9607e4b140fcf44e674b
Carlos Ali Schutz <carlos.schutz@cwi.com.br>
2023-08-02 10:07:27 -0300

    adicionando projeto

---
+++ README.md
@@ -0,0 +1,9 @@
+# Formulário com Validação
+
+Formulário com validação por campo.
+
+https://carlosalischutz.github.io/Formulario-Validacao/
+
+![Layout Foto](https://github.com/CarlosAliSchutz/Formulario-Validacao/blob/main/src/layout/Form.jpg)
+![Layout Foto](https://github.com/CarlosAliSchutz/Formulario-Validacao/blob/main/src/layout/Form-completed.jpg)
+![Layout Foto](https://github.com/CarlosAliSchutz/Formulario-Validacao/blob/main/src/layout/Form-empty.jpg)
---
+++ src/js/send.js
@@ -0,0 +1,23 @@
+  const form = document.getElementById("form-send");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const fields = form.querySelectorAll('input[type="text"], textarea');
+
+    fields.forEach((field) => {
+      if (field.value.trim() === "") {
+        field.classList.add("text-required");
+        const span = field.nextElementSibling;
+        if (span.classList.contains("required")) {
+          span.classList.add("ativo");
+        }
+      } else {
+        field.classList.add("field");
+        const span = field.nextElementSibling;
+        if (span.classList.contains("required")) {
+          span.classList.remove("ativo");
+        }
+      }
+    });
+  });
